36a106dfb767938649023dd6b8a837d85dbfd98f
Aly Raffauf <aly@aly.codes>
2025-11-10 09:30:14 -0500

    nix: update buildMachines

--- modules/snippets/nix/buildMachines.nix
+++ modules/snippets/nix/buildMachines.nix
@@ -6,30 +6,10 @@
 
       default = [
         {
-          hostName = "lavaridge";
-          maxJobs = 8;
-          protocol = "ssh-ng";
-          speedFactor = 3;
-          sshKey = "/etc/ssh/ssh_host_ed25519_key";
-          sshUser = "nixbuild";
-          supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-          systems = ["x86_64-linux"];
-        }
-        {
-          hostName = "lilycove";
+          hostName = "jubilife";
           maxJobs = 12;
           protocol = "ssh-ng";
           speedFactor = 5;
-          sshKey = "/etc/ssh/ssh_host_ed25519_key";
-          sshUser = "nixbuild";
-          supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-          systems = ["x86_64-linux"];
-        }
-        {
-          hostName = "mauville";
-          maxJobs = 4;
-          protocol = "ssh-ng";
-          speedFactor = 1;
           sshKey = "/etc/ssh/ssh_host_ed25519_key";
           sshUser = "nixbuild";
           supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
